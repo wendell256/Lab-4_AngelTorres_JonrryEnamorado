92c7d32fe4d52b2b31032ca95541934314dab6af
Jonrry <jonrry.enamorado@unitec.edu>
2018-05-18 13:27:01 -0600

    Creacion de clase piezas

---
+++ Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Piezas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg4_angeltorres_jonrryenamorado;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author wende
+ */
+public class Piezas {
+    Color color;
+    String material;
+    
+    
+}
