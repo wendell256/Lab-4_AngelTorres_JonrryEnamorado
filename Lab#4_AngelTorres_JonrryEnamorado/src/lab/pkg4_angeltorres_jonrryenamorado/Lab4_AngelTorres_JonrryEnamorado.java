e579ffcf8d4c8293d4f8593557fcf66f4d042080
Jonrry <jonrry.enamorado@unitec.edu>
2018-05-18 15:47:52 -0600

    Excepcion

--- Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Lab4_AngelTorres_JonrryEnamorado.java
+++ Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Lab4_AngelTorres_JonrryEnamorado.java
@@ -12,43 +12,45 @@ public class Lab4_AngelTorres_JonrryEnamorado {
         Scanner sc = new Scanner(System.in);
         ArrayList<Jugadores> jugador = new ArrayList();
         int menu = -1;
-        
 
-        try {//Try-Catch menú principal
-            do {
-                System.out.println("A.M.A \n"
-                        + "1 - Administrar Jugadores\n"
-                        + "2 - Nueva Partida\n"
-                        + "0 - Salir\n");
-                menu = sc.nextInt();
+        //try {//Try-Catch menú principal
+        do {
+            sc = new Scanner(System.in);
+            System.out.println("A.M.A \n"
+                    + "1 - Administrar Jugadores\n"
+                    + "2 - Nueva Partida\n"
+                    + "0 - Salir\n");
+            menu = sc.nextInt();
 
-                switch (menu) {
-                    case 1://Agregar, listar, eliminar jugadores
-                        int menuJ = -1;
+            switch (menu) {
+                case 1://Agregar, listar, eliminar jugadores
+                    int menuJ = -1;
 
-                        try {
-                            do{
-                            System.out.println("Administrar Jugadores\n"
-                                    + "1 - Agregar\n"
-                                    + "2 - Listar\n"
-                                    + "3 - Eliminar\n"
-                                    + "0 - Regresar\n");
-                            menuJ = sc.nextInt();
+                    // try {
+                    do {
+                        sc = new Scanner(System.in);
+                        System.out.println("Administrar Jugadores\n"
+                                + "1 - Agregar\n"
+                                + "2 - Listar\n"
+                                + "3 - Eliminar\n"
+                                + "0 - Regresar\n");
+                        menuJ = sc.nextInt();
 
-                            switch (menuJ) {//Menu administrar jugadores
-                                
-                                case 1://Agregar jugador
-                                    String nombre,
-                                     id,
-                                     sex,
-                                     birth;
-                                    int edad;
-                                    boolean f=true;
+                        switch (menuJ) {//Menu administrar jugadores
 
+                            case 1://Agregar jugador
+                                String nombre,
+                                 id,
+                                 sex,
+                                 birth;
+
+                                boolean f = true;
+
+                                while (f) {
                                     try {
-                                        while(f){
                                         System.out.println("Edad: ");
-                                        edad = sc.nextInt();
+                                        sc = new Scanner(System.in);
+                                        int edad = sc.nextInt();
 
                                         System.out.println("Nombre: ");
                                         nombre = sc.next();
@@ -64,76 +66,73 @@ public class Lab4_AngelTorres_JonrryEnamorado {
 
                                         jugador.add(new Jugadores(nombre, id, birth, edad, sex));
                                         System.out.println("Jugador agregado exitosamente.");
-                                        f=false;
-                                        }
+                                        f = false;
 
                                     } catch (InputMismatchException e) {
-                                        System.err.println("Debe ingresar un entero!");
-                                        e.printStackTrace();
-                                        f=true;
+                                        System.out.println("Debe ingresar un entero!");
+                                        // e.printStackTrace();
+                                        f = true;
+                                        sc.reset();
                                     }
+                                }
 
-                                    break;
+                                break;
 
-                                case 2://Listar jugadores
-                                    System.out.println("Lista de Jugadores\n");
+                            case 2://Listar jugadores
+                                System.out.println("Lista de Jugadores\n");
 
-                                    String salida = "";
-                                    for (Object temp : jugador) {
-                                        if (temp instanceof Jugadores) {
-                                            salida += temp + "\n";
-                                        }
+                                String salida = "";
+                                for (Object temp : jugador) {
+                                    if (temp instanceof Jugadores) {
+                                        salida += temp + "\n";
                                     }
-                                    System.out.println(salida);
+                                }
+                                System.out.println(salida);
 
-                                    break;
-                                case 3://Eliminar Jugadores
-                                    System.out.println("Eliminar Jugadores\n");
-                                    int pos;
-                                    System.out.println("Ingrese la posición: ");
-                                    pos = sc.nextInt();
+                                break;
+                            case 3://Eliminar Jugadores
+                                System.out.println("Eliminar Jugadores\n");
+                                int pos;
+                                System.out.println("Ingrese la posición: ");
+                                pos = sc.nextInt();
 
-                                    jugador.remove(pos);
-                                    break;
+                                jugador.remove(pos);
+                                break;
 
-                            }//Menu administrar jugadores
-                            
-                            }while(menuJ!=0); 
+                        }//Menu administrar jugadores
 
-                        } catch (Exception e) {
+                    } while (menuJ != 0);
+
+                    /*} catch (Exception e) {
                             e.printStackTrace();
-                        }
 
-                        break;
-                        
-                    case 2:
-                        int pos1,
-                         pos2;
-                        System.out.println("Nueva partida\n");
+                        }*/
+                    break;
 
-                        System.out.println("Ingrese posición jugador 1: ");
-                        pos1 = sc.nextInt();
+                case 2:
+                    int pos1,
+                     pos2;
+                    System.out.println("Nueva partida\n");
 
-                        System.out.println("Ingrese posición jugador 2");
-                        pos2 = sc.nextInt();
+                    System.out.println("Ingrese posición jugador 1: ");
+                    pos1 = sc.nextInt();
 
-                        break;
-                        
-                    case 3:
+                    System.out.println("Ingrese posición jugador 2");
+                    pos2 = sc.nextInt();
 
-                        break;
-                        
-                }
-            } while (menu != 0);
-            
-           
-        } catch (Exception e) {
+                    break;
+
+                case 3:
+
+                    break;
+
+            }
+        } while (menu != 0);
+
+        /*} catch (Exception e) {
             e.printStackTrace();
-            
-        }//Final Try-Catch menú principal
-        
-        
 
+        }//Final Try-Catch menú principal*/
     }
 
 }
