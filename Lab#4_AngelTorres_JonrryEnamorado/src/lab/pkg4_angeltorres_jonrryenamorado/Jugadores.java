58c1c85bfbde9c97c65b0b9805ebc07f3cc1295a
Jonrry <jonrry.enamorado@unitec.edu>
2018-05-18 13:21:29 -0600

    Creacion de clase jugadores

--- Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Jugadores.java
+++ Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Jugadores.java
@@ -5,10 +5,84 @@
  */
 package lab.pkg4_angeltorres_jonrryenamorado;
 
+import java.util.Date;
+
 /**
  *
  * @author wende
  */
 public class Jugadores {
+    private String nombre;
+    private String id;
+    private int puntos;
+    private Date birth;
+    private int edad;
+    private String sex;
+
+    public Jugadores() {
+    }
+
+    public Jugadores(String nombre, String id, int puntos, Date birth, int edad, String sex) {
+        this.nombre = nombre;
+        this.id = id;
+        this.puntos = puntos;
+        this.birth = birth;
+        this.edad = edad;
+        this.sex = sex;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugadores{" + "nombre=" + nombre + ", id=" + id + ", puntos=" + puntos + ", birth=" + birth + ", edad=" + edad + ", sex=" + sex + '}';
+    }
+    
     
 }
