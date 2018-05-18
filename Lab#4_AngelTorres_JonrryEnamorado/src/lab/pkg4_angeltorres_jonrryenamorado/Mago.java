77fbd66a52060c6de32ba28a98f8a0ac22b3446e
antox89 <ator89@unitec.edu>
2018-05-18 17:34:48 -0600

    Mago

--- Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Mago.java
+++ Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Mago.java
@@ -6,12 +6,12 @@
 package lab.pkg4_angeltorres_jonrryenamorado;
 
 import java.awt.Color;
+import java.util.Scanner;
 
-/**
- *
- * @author wende
- */
 public class Mago extends Piezas{
+    
+    
+    
 
     public Mago(Color color) {
         super(color);
@@ -22,7 +22,29 @@ public class Mago extends Piezas{
 
     @Override
     public void mover(int x, int y, Tablero t, int x2, int y2, Color c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while(!validar(x,y,x2,y2)){
+            System.out.println("Posición de pieza no válida");
+            System.out.println("Ingrese X: ");
+            x2=sc.nextInt();
+            System.out.println("Ingrese Y:");
+            y2=sc.nextInt();
+        }
+        
+        t.tablero[x2][y2] = t.tablero[x][y];
+        t.tablero[x][y] = "0";
+        String tmp = t.tvisible[x][y];
+        t.tvisible[x2][y2] = tmp;
+        t.tvisible[x][y] = "__";
+    }
+    
+    public boolean validar(int x, int y, int x2, int y2){
+        if((x==x2) || (y==y2) || (Math.abs(x-x2) == Math.abs(y-y2))){
+            return true;
+        }
+            return false;
     }
     
 }
