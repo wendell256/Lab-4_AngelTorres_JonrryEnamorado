69846ed88fe255d63f95757be560bb5409fd90e3
Jonrry <jonrry.enamorado@unitec.edu>
2018-05-18 17:34:11 -0600

    update

--- Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Duende.java
+++ Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Duende.java
@@ -6,6 +6,7 @@
 package lab.pkg4_angeltorres_jonrryenamorado;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 /**
  *
@@ -21,15 +22,41 @@ public class Duende extends Piezas {
     }
     
     
-    
+    Scanner sc = new Scanner(System.in);
     @Override
     public void mover (int x, int y, Tablero t, int x2, int y2, Color c){
         
         if(c == Color.BLACK){
-            
+            while(!validarn(x,y,x2,y2)){
+                System.out.println("Posicion No valida para mover");
+            System.out.println("Ingrese x a mover pieza: ");
+            x2 = sc.nextInt();
+            System.out.println("Ingrese y a mover pieza: ");
+            y2 = sc.nextInt();
+            }
+        }
+        else{
+            while(!validarb(x,y,x2,y2)){
+                System.out.println("Posicion No valida para mover");
+            System.out.println("Ingrese x a mover pieza: ");
+            x2 = sc.nextInt();
+            System.out.println("Ingrese y a mover pieza: ");
+            y2 = sc.nextInt();
+            }
         }
         
     }
-
+public boolean validarn(int x, int y, int x2, int y2){
+    if(x2-x == 1 && y == y2){
+        return true;
+    }
+    return false;
+}
+public boolean validarb(int x, int y, int x2, int y2){
+    if(x2-x == -1 && y == y2){
+        return true;
+    }
+    return false;
+}
     
 }
