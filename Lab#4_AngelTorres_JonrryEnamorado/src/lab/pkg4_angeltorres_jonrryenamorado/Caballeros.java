79761954666f93fb2ad1f375be043195aaa6d5b3
Jonrry <jonrry.enamorado@unitec.edu>
2018-05-18 13:59:14 -0600

    Tablero

--- Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Caballeros.java
+++ Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Caballeros.java
@@ -5,10 +5,20 @@
  */
 package lab.pkg4_angeltorres_jonrryenamorado;
 
+import java.awt.Color;
+
 /**
  *
  * @author wende
  */
 public class Caballeros extends Piezas{
+
+    public Caballeros() {
+    
+    }
+
+    public Caballeros(Color color, String material) {
+        super(color, material);
+    }
     
 }
---
+++ Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Tablero.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg4_angeltorres_jonrryenamorado;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author wende
+ */
+public class Tablero {
+    private String[][] tablero;
+    private ArrayList Blancas;
+    private ArrayList Negras;
+
+    public Tablero() {
+    tablero = new String[10][10];
+    //agregar piezas
+    Blancas.add(new Caballeros());
+    Blancas.add(new Caballeros());
+    Blancas.add(new Caballeros());
+    //
+    Blancas.add(new Duende());
+    Blancas.add(new Duende());
+    Blancas.add(new Duende());
+    Blancas.add(new Duende());
+    //
+    Blancas.add(new Arquero());
+    Blancas.add(new Arquero());
+    Blancas.add(new Arquero());
+    Blancas.add(new Arquero());
+    //
+    Blancas.add(new Mago());
+    //
+    Blancas.add(new Dragon());
+    Blancas.add(new Dragon());
+    //
+    Blancas.add(new Rey());
+    //
+    Negras.add(new Caballeros());
+    Negras.add(new Caballeros());
+    Negras.add(new Caballeros());
+    //
+    Negras.add(new Duende());
+    Negras.add(new Duende());
+    Negras.add(new Duende());
+    Negras.add(new Duende());
+    //
+    Negras.add(new Arquero());
+    Negras.add(new Arquero());
+    Negras.add(new Arquero());
+    Negras.add(new Arquero());
+    //
+    Negras.add(new Mago());
+    //
+    Negras.add(new Dragon());
+    Negras.add(new Dragon());
+    //
+    Negras.add(new Rey());
+    }
+
+    @Override
+    public String toString() {
+        return "Tablero{" + "tablero=" + tablero + ", Blancas=" + Blancas + ", Negras=" + Negras + '}';
+    }
+    
+    
+}
