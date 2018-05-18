9e2fcd4f839980fef0e7e1859fa4c2be86738760
Jonrry <jonrry.enamorado@unitec.edu>
2018-05-18 13:36:33 -0600

    Creacion de las diferentes clases

---
+++ Lab#4_AngelTorres_JonrryEnamorado/src/lab/pkg4_angeltorres_jonrryenamorado/Rey.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg4_angeltorres_jonrryenamorado;
+
+/**
+ *
+ * @author wende
+ */
+public class Rey extends Piezas{
+    
+}
